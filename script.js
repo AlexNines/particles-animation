011232f2afaf2aed30a33cc21f85c66f29e0b8aa
AlexNines <p39q10m4@gmail.com>
2020-08-26 16:15:20 +0400

    dots, lines

--- script.js
+++ script.js
@@ -10,7 +10,8 @@
             particleColor: 'rgba(255,40,40,1)',
             particleRadius: 3,
             particleCount: 60,
-            particleMaxVelocity: 0.5
+            particleMaxVelocity: 0.5,
+            lineLegth: 150
         }
     document.querySelector('body').appendChild(canvas)
 
@@ -23,13 +24,13 @@
         constructor() {
             this.x = Math.random() * w
             this.y = Math.random() * h
-            this.velocityX = Math.random()*(properties.particleMaxVelocity*2)-properties.particleMaxVelocity 
-            this.velocityY = Math.random()*(properties.particleMaxVelocity*2)-properties.particleMaxVelocity 
+            this.velocityX = Math.random() * (properties.particleMaxVelocity * 2) - properties.particleMaxVelocity
+            this.velocityY = Math.random() * (properties.particleMaxVelocity * 2) - properties.particleMaxVelocity
 
         }
-        position(){
-            this.x + this.velocityX > w && this.velocityX > 0 || this.x + this.velocityX < 0 && this.velocityX< 0 ? this.velocityX*=-1 : this.velocityX
-            this.y + this.velocityY > w && this.velocityY > 0 || this.y + this.velocityY < 0 && this.velocityY< 0 ? this.velocityY*=-1 : this.velocityY
+        position() {
+            this.x + this.velocityX > w && this.velocityX > 0 || this.x + this.velocityX < 0 && this.velocityX < 0 ? this.velocityX *= -1 : this.velocityX
+            this.y + this.velocityY > w && this.velocityY > 0 || this.y + this.velocityY < 0 && this.velocityY < 0 ? this.velocityY *= -1 : this.velocityY
 
             this.x += this.velocityX
             this.y += this.velocityY
@@ -44,6 +45,31 @@
         }
     }
 
+    function drawLines() {
+        let x1, y1, x2, y2, length, opacity
+        for (let i in particles) {
+            for (let j in particles) {
+                x1 = particles[i].x
+                y1 = particles[i].y
+                x2 = particles[j].x
+                y2 = particles[j].y
+                length = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2))
+                if (length < properties.lineLegth) {
+                    opacity = 1 - length/properties.lineLegth
+                    ctx.lineWidth = '0,5'
+                    ctx.strokeStyle = 'rgba(255, 40,40, '+opacity+')'
+                    ctx.beginPath()
+                    ctx.moveTo(x1, y1)
+                    ctx.lineTo(x2, y2)
+                    ctx.closePath()
+                    ctx.stroke()
+
+                }
+
+            }
+        }
+
+    }
 
     function reDrawBackground() {
         ctx.fillStyle = properties.bgColor
@@ -56,10 +82,11 @@
             particles[i].reDraw()
         }
     }
-    
+
     function loop() {
         reDrawBackground()
         reDrawParticles()
+        drawLines()
         requestAnimationFrame(loop)
     }
 
