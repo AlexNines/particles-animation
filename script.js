acd518efe22e1c29d86180e5a55e9109cd470a91
AlexNines <p39q10m4@gmail.com>
2020-08-26 15:50:33 +0400

    dots hive)

--- script.js
+++ script.js
@@ -3,46 +3,72 @@
     let canvas = document.createElement('canvas'),
         ctx = canvas.getContext('2d'),
         w = canvas.width = innerWidth,
-        h = canvas.innerHeigth,
+        h = canvas.height = innerHeight,
         particles = [],
         properties = {
             bgColor: 'rgba(17,17,19,1)',
-particleColor: 'rgba(255,40,40,1)',
-particleRadius: 3,
-particleCount: 60
+            particleColor: 'rgba(255,40,40,1)',
+            particleRadius: 3,
+            particleCount: 60,
+            particleMaxVelocity: 0.5
         }
     document.querySelector('body').appendChild(canvas)
 
     window.onresize = function () {
         w = canvas.width = innerWidth,
-            h = canvas.innerHeigth
+            h = canvas.height = innerHeight
     }
 
-class Particle{
-    constructor(){
-        this.x = Math.random()*w
-        this.y = Math.random()*h
+    class Particle {
+        constructor() {
+            this.x = Math.random() * w
+            this.y = Math.random() * h
+            this.velocityX = Math.random()*(properties.particleMaxVelocity*2)-properties.particleMaxVelocity 
+            this.velocityY = Math.random()*(properties.particleMaxVelocity*2)-properties.particleMaxVelocity 
+
+        }
+        position(){
+            this.x + this.velocityX > w && this.velocityX > 0 || this.x + this.velocityX < 0 && this.velocityX< 0 ? this.velocityX*=-1 : this.velocityX
+            this.y + this.velocityY > w && this.velocityY > 0 || this.y + this.velocityY < 0 && this.velocityY< 0 ? this.velocityY*=-1 : this.velocityY
+
+            this.x += this.velocityX
+            this.y += this.velocityY
+
+        }
+        reDraw() {
+            ctx.beginPath()
+            ctx.arc(this.x, this.y, properties.particleRadius, 0, Math.PI * 2)
+            ctx.closePath()
+            ctx.fillStyle = properties.particleColor
+            ctx.fill()
+        }
     }
-    reDraw(){
-        ctx.beginPath()
-       ctx.arc(this.x, this.y, properties.particleRadius, 0, Math.PI*2)
-       ctx.closePath()
-        
-    }
-}
 
 
-    function redrawBackground() {
+    function reDrawBackground() {
         ctx.fillStyle = properties.bgColor
         ctx.fillRect(0, 0, w, h)
     }
 
+    function reDrawParticles() {
+        for (let i in particles) {
+            particles[i].position()
+            particles[i].reDraw()
+        }
+    }
+    
     function loop() {
-        redrawBackground()
+        reDrawBackground()
+        reDrawParticles()
         requestAnimationFrame(loop)
     }
 
     function init() {
+        for (let i = 0; i < properties.particleCount; i++) {
+            particles.push(new Particle)
+        }
         loop()
+
     }
+    init()
 }())
